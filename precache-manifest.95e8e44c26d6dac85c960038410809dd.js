c10716d41a0f91a20ff1961c0247c8fe847dee9b
tsaketh <tsaketh1107@gmail.com>
2021-02-14 17:09:40 +0530

    Updates

---
+++ precache-manifest.95e8e44c26d6dac85c960038410809dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61236a08720bf8766cb05221a73a70ea",
+    "url": "/face-detect-ai/index.html"
+  },
+  {
+    "revision": "230f6d77ab8962e5b1f1",
+    "url": "/face-detect-ai/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2f2116100e032a7161f3",
+    "url": "/face-detect-ai/static/css/main.3f1da22e.chunk.css"
+  },
+  {
+    "revision": "230f6d77ab8962e5b1f1",
+    "url": "/face-detect-ai/static/js/2.192f0b18.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-detect-ai/static/js/2.192f0b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f2116100e032a7161f3",
+    "url": "/face-detect-ai/static/js/main.b433f654.chunk.js"
+  },
+  {
+    "revision": "27f73f6587f754628f3e",
+    "url": "/face-detect-ai/static/js/runtime-main.068e2f7a.js"
+  },
+  {
+    "revision": "720fcd37362746256cf1a6db9f3e0a3d",
+    "url": "/face-detect-ai/static/media/MYprofile.720fcd37.jpg"
+  }
+]);
